08c6502a40b98ef8622b924f5d149d29ccf925f6
Minghang <lily88zym@gmail.com>
2022-01-19 17:33:34 -0500

    reminder

--- src/App.js
+++ src/App.js
@@ -23,10 +23,26 @@ const App = () => {
         reminder: false,
     }
 ])
+
+// Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? {...task, reminder: !task.reminder} : task
+    )
+  )
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length>0 ? (<Tasks tasks={tasks} 
+      onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks To Show')}
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,13 @@
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
 
 
-const Tasks = ({tasks}) => {
-    
-    
     return (
         <>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>
+                <Task key={task.id} task={task} 
+                onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )
